f32cb4f19faec9d2708bf3813666d134edb741fb
tim <tim.vandaalen@wur.nl>
2024-08-18 18:19:12 +0200

    added winrar zipping support

--- iRODS_ingest/zipper.py
+++ iRODS_ingest/zipper.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 from time import sleep
 from zipfile import ZipFile, BadZipFile
+from subprocess import run, CalledProcessError, PIPE
 
 
 class ZipperProcess(multiprocessing.Process):
@@ -23,6 +24,11 @@ class ZipperProcess(multiprocessing.Process):
         self.free_diskspace = free_diskspace
         self.id = id
 
+        # check for winrar
+        self.winrar_path = self.get_winrar_path()
+        if self.winrar_path:
+            logging.info("WinRAR detected")
+
     def run(self):
         while not self.stop_worker.is_set():
             row_dict = self.files_to_zip_queue.get()
@@ -40,17 +46,84 @@ class ZipperProcess(multiprocessing.Process):
                     logging.info("%d Not enough free diskspace, waiting for more", self.id)
                     sleep(300)
                 start_time = datetime.now()
-                # Shutil adds zip extension automatically
-                zip_root, _ = os.path.splitext(row_dict['_zipPath'])
-                shutil.make_archive(zip_root, 'zip', row_dict['_Path'])
+                if self.winrar_path:
+                    status = self.zip_file_with_winrar(self.winrar_path, row_dict['_Path'], row_dict['_zipPath'])
+                else:
+                    status = self.zip_file_with_shutil(row_dict['_Path'], row_dict['_zipPath'])
                 logging.info(f"Zipper {self.id} zipped {row_dict['_Path']} in {datetime.now() - start_time}")
-                if self.check_zip(row_dict['_zipPath']):
+                if status and self.check_zip(row_dict['_zipPath']):
                     self.zipped_files_queue.put(row_dict)
                 else:
                     logging.error(f"Zipper {self.id} failed to zip {row_dict['_Path']}")
                     exit(1)
             except Exception as e:
                 logging.error(f"Error zipping file {row_dict['_Path']}: {e}")
+
+    def get_winrar_path(self) -> str:
+        """Get the installation path of WinRAR by checking common directories."""
+        # windows
+        common_paths = [
+            r"C:\Program Files\WinRAR\winrar.exe",
+            r"C:\Program Files (x86)\WinRAR\winrar.exe"
+        ]
+        for path in common_paths:
+            if os.path.exists(path):
+                logging.info(f"WinRAR is installed at: {path}")
+                return path
+
+        # Ubuntu
+        try:
+            result = run(["where", "rar"], stdout=PIPE, stderr=PIPE, text=True)
+            if result.returncode == 0:
+                winrar_path = result.stdout.strip()
+                logging.info(f"WinRAR is installed at: {winrar_path}")
+                return winrar_path + os.sep + "winrar"
+            else:
+                logging.error("WinRAR is not installed or not found in PATH.")
+                return ""
+        except FileNotFoundError:
+            logging.error("The 'where' command is not found.")
+        return ""
+
+    def zip_file_with_winrar(self, rar_path, local_path, zip_path) -> bool:
+        """Zip a file using WinRAR
+        Args:
+            rar_path: str
+                path to the WinRAR executable
+            local_path: str
+                path to the file to zip
+            zip_path: str
+                path to the zip file
+        Returns:
+            bool: True if succesfull
+        """
+        try:
+            # Construct the WinRAR command, -inul is for no output
+            command = [
+                rar_path, "a", "-afzip" "-ep1",  "-inul", str(zip_path), str(local_path)
+            ]
+            # Execute the command
+            run(command, check=True)
+            logging.info(f"Successfully zipped {local_path} to {zip_path}")
+            return True
+        except CalledProcessError as e:
+            logging.error(f"Failed to zip file {local_path}: {e}")
+        return False
+
+    def zip_file_with_shutil(self, local_path: str, zip_path: str) -> bool:
+        """Zip a file using shutil
+        Args:
+            local_path: str
+                path to the file to zip
+            zip_path: str
+                path to the zip file
+        Returns:
+            bool: True if succesfull
+        """
+        # Shutil adds zip extension automatically
+        zip_root, _ = os.path.splitext(zip_path)
+        shutil.make_archive(zip_root, 'zip', local_path)
+        return True
 
     def check_zip(self, zip_path: str) -> bool:
         """Check if the zip file is valid
