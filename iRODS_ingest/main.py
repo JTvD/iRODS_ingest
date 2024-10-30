5df4e951132bcac4c6690e69075fd9281ac840ca
tim <tim.vandaalen@wur.nl>
2024-10-30 14:14:32 +0100

    flake8

--- iRODS_ingest/main.py
+++ iRODS_ingest/main.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="Script to process and upload files.")
     parser.add_argument('--config', type=str, required=False, help='Path to the config file')
-    parser.add_argument('-t', '--totape', dest='totape', default=False, required=False, 
+    parser.add_argument('-t', '--totape', dest='totape', default=False, required=False,
                         action="store_true", help='Add this flag to send files to tape')
     args = parser.parse_args()
 
